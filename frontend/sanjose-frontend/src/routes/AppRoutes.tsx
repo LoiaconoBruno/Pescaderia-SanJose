6cd851f90e696a6ad274a9d1ac07ec4fd63bb9f3
Bruno <loiaconobruno08@gmail.com>
2026-01-19 22:50:00 -0300

    F

--- frontend/sanjose-frontend/src/routes/AppRoutes.tsx
+++ frontend/sanjose-frontend/src/routes/AppRoutes.tsx
@@ -1,7 +1,7 @@
-import { Routes, Route, Navigate } from "react-router-dom"; // ← Agregá Routes
+// AppRoutes.tsx - ACÁ ponemos el Router
+import { Routes, Route, Navigate, HashRouter } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 import PrivateLayout from "../layouts/PrivateLayout";
-
 import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
 import Productos from "../pages/Productos";
@@ -10,23 +10,20 @@ import Salidas from "../pages/Salidas";
 
 export default function AppRoutes() {
   return (
-    <Routes>
-      {/* Públicas */}
-      <Route path="/login" element={<Login />} />
-
-      {/* Privadas */}
-      <Route element={<PrivateRoute />}>
-        <Route element={<PrivateLayout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/productos" element={<Productos />} />
-          <Route path="/entradas" element={<Entradas />} />
-          <Route path="/salidas" element={<Salidas />} />
+    <HashRouter> {/* ← Ponelo ACÁ */}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route element={<PrivateRoute />}>
+          <Route element={<PrivateLayout />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/productos" element={<Productos />} />
+            <Route path="/entradas" element={<Entradas />} />
+            <Route path="/salidas" element={<Salidas />} />
+          </Route>
         </Route>
-      </Route>
-
-      {/* Default */}
-      <Route path="/" element={<Navigate to="/dashboard" replace />} />
-      <Route path="*" element={<Navigate to="/dashboard" replace />} />
-    </Routes>
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route path="*" element={<Navigate to="/dashboard" replace />} />
+      </Routes>
+    </HashRouter>
   );
 }
