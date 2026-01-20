0892b6e351393a9e3b603eea1c7c44c57547a3ee
Bruno <loiaconobruno08@gmail.com>
2026-01-19 21:54:00 -0300

    FIX

--- frontend/sanjose-frontend/src/main.tsx
+++ frontend/sanjose-frontend/src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <HashRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </HashRouter>
   </React.StrictMode>
 );
